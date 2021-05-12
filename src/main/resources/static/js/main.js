dc8d4f85a427638d2f6a15f46b3508f899e2de6f
rossyrom84 <rosangely.romero@gmail.com>
2021-05-12 12:59:04 +0200

    javascript

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,17 @@
+let addToDoButton = document.getElementById('addhomework');
+let ToDoListaUl = document.getElementById('lista-ul');
+let inputField = document.getElementById('inputfield');
+
+
+function addToList() {
+    let doing = document.createElement('li');
+    doing.innerText = inputField.value;
+    ToDoListaUl.appendChild(doing);
+    inputField.value = "";
+ }
+
+addToDoButton.addEventListener('click', addToList);
+
+
+
+
