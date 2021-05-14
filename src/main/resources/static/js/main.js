38e07293b9e902153ad6d5b90069b49c5141a755
Yeraldsb <pamelasb9412@gmail.com>
2021-05-14 10:43:03 +0200

    Yeraldin añade funcion loadRequest

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,12 +4,19 @@ let inputField = document.getElementById('inputfield');
 
 
 function addToList() {
+
     let doing = document.createElement('li');
     doing.innerText = inputField.value;
     ToDoListaUl.appendChild(doing);
     inputField.value = "";
  }
 
+ function loadRequest(){
+    fetch("/homeworks")
+        .then(response => response.json())
+        .then(addToList)
+ }
+ document.addEventListener("DOMContentLoaded", loadRequest);
 addToDoButton.addEventListener('click', addToList);
 
 
