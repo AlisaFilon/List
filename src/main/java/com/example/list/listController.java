1a6c74beac61ede260942ef335cea10344008454
rossyrom84 <rosangely.romero@gmail.com>
2021-05-20 11:47:07 +0200

    delete

--- src/main/java/com/example/list/listController.java
+++ src/main/java/com/example/list/listController.java
@@ -1,8 +1,6 @@
 package com.example.list;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
@@ -14,17 +12,20 @@ public class listController {
     private final List<Lista> homeworks = new ArrayList<>();
 
     @GetMapping("/homeworks")
-    public List<Lista> allhomeworks() {
+    public List<Lista> allHomeworks() {
         return homeworks;
     }
 
     @PostMapping("/homeworks")
-    public RedirectView addNewHomework(Lista homework) {
-        homeworks.add(homework);
+    public RedirectView addNewHomework(Lista lista) {
+        homeworks.add(lista);
         return new RedirectView("/");
     }
 
-
+    @DeleteMapping("/homeworks/{index}")
+    void deleteHomework(@PathVariable int index) {
+        homeworks.remove(index);
+    }
 
 }
 
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,29 +1,31 @@
 const homeworksSection = document.querySelector("#homework-section");
 
-function showHomeworks(text) {
-    text.forEach(Lista => {
-        const element = `<div class="tarea">
+function showHomeworks(homeworks) {
+    homeworksSection.innerHTML = "";
+    homeworks.forEach(Lista, position) => {
+        const htmlElement = document.createElement("div");
+        htmlElement.innerHTML = `<div class="tarea">
+            <div class="close-button"
+            onclick="fetch(\`/homeworks/${position}\`, {method: 'DELETE'}).then(reloadHomeworks)">x</div>
             <p class="toDo">${Lista.homework}</p>
         </div>`;
-        homeworksSection.insertAdjacentHTML("beforeend", element)
-    })
+        homeworksSection.appenChild(htmlElement)
+    }
  }
 
- function loadRequest(){
+ let reloadHomeworks = () => {
     fetch("/homeworks")
-        .then(response => response.json())
+        .then(r => r.json())
         .then(showHomeworks)
-
-
  };
 
-document.addEventListener("DOMContentLoaded", loadRequest);
+document.addEventListener("DOMContentLoaded", reloadHomeworks());
+
+
 
 const toDoInput = document.getElementById("homework");
-
-
-
 const submitButton = document.getElementById("homework-submit");
+
 const validateHomework = (event) => {
     const homework = event.target.value;
     if (homework.length < 3) {
@@ -33,7 +35,6 @@ const validateHomework = (event) => {
     }
 }
 toDoInput.addEventListener("input" , validateHomework);
-
 
 
 
